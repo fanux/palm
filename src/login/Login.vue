55ceefd0a1958a9db2f6ff48f04cc60e4f78af69
fanux <474785153@qq.com>
2019-03-15 23:05:17 +0800

    add login and terminal

---
+++ src/login/Login.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+    <Row>
+      <Col span="8">col-8</Col>
+      <Col span="8">col-8</Col>
+      <Col span="8">
+        <Form ref="formInline" :model="formInline" :rules="ruleInline" class="loginForm">
+          <FormItem prop="user">
+            <Input type="text" v-model="formInline.user" placeholder="Username">
+              <Icon type="ios-person-outline" slot="prepend"></Icon>
+            </Input>
+          </FormItem>
+          <FormItem prop="password">
+            <Input type="password" v-model="formInline.password" placeholder="Password">
+              <Icon type="ios-lock-outline" slot="prepend"></Icon>
+            </Input>
+          </FormItem>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('formInline')">Signin</Button>
+          </FormItem>
+        </Form>
+      </Col>
+    </Row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      formInline: {
+        user: "",
+        password: ""
+      },
+      ruleInline: {
+        user: [
+          {
+            required: true,
+            message: "Please fill in the user name",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "Please fill in the password.",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            min: 6,
+            message: "The password length cannot be less than 6 bits",
+            trigger: "blur"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    handleSubmit(name) {
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.$Message.success("Success!");
+          this.$router.push('terminal')
+        } else {
+          this.$Message.error("Fail!");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.loginForm {
+  margin-right: 120px;
+}
+</style>
