b921a7748db373344e923dfb29d09167960fab60
fanux <fhtjob@hotmail.com>
2019-03-19 21:10:44 +0800

    add create token with https

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export default {
     data() {
         return {
-            fistRBACServer: "http://fist.lameleg.com:32203/login"
+            fistRBACServer: "http://fist.lameleg.com:32203/login",
+            fistAuthServerToken: "https://fist.lameleg.com:32201/token"
         }
     }
 }
